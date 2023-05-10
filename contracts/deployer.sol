88a7ba09d3f64eacc545c887680e0b653392dde6
Abhishek Kumar <abhishekkumar35962@gmail.com>
2023-05-10 15:17:43 +0530

    fix: new deployed

--- contracts/deployer.sol
+++ contracts/deployer.sol
@@ -9,67 +9,70 @@ import "./erc20burner.sol";
 import "./nativeburner.sol";
 
 contract PurplePayBurnerDeployer is Ownable {
-    using SafeMath for uint;
+	using SafeMath for uint;
 
-    event BurnerContractDeployed(address burnerContract);
+	event BurnerContractDeployed(
+		address burnerContract,
+		address merchantAddress
+	);
 
-    function deploy(
-        string memory _salt,
-        address _erc20Token,
-        uint _amount,
-        address _merchantAddress,
-        address _purplePayMultiSig
-    ) public onlyOwner returns (address) {
-        if (_erc20Token != address(0)) {
-            ERC20BurnerContract erc20Burner = new ERC20BurnerContract{
-                salt: bytes32(keccak256(abi.encodePacked(_salt)))
-            }(_erc20Token, _amount, _merchantAddress, _purplePayMultiSig);
-            emit BurnerContractDeployed(address(erc20Burner));
-            return address(erc20Burner);
-        }
+	function deploy(
+		string memory _salt,
+		address _erc20Token,
+		uint _amount,
+		address _merchantAddress,
+		address _purplePayMultiSig
+	) public onlyOwner returns (address) {
+		if (_erc20Token != address(0)) {
+			ERC20BurnerContract erc20Burner = new ERC20BurnerContract{
+				salt: bytes32(keccak256(abi.encodePacked(_salt)))
+			}(_erc20Token, _amount, _merchantAddress, _purplePayMultiSig);
+			emit BurnerContractDeployed(address(erc20Burner), _merchantAddress);
+			return address(erc20Burner);
+		}
 
-        NativeBurnerContract nativeBurner = new NativeBurnerContract{
-            salt: bytes32(keccak256(abi.encodePacked(_salt)))
-        }(_amount, _merchantAddress, _purplePayMultiSig);
-        emit BurnerContractDeployed(address(nativeBurner));
-        return address(nativeBurner);
-    }
+		NativeBurnerContract nativeBurner = new NativeBurnerContract{
+			salt: bytes32(keccak256(abi.encodePacked(_salt)))
+		}(_amount, _merchantAddress, _purplePayMultiSig);
+		emit BurnerContractDeployed(address(nativeBurner), _merchantAddress);
+		return address(nativeBurner);
+	}
 
-    function predictAddress(
-        string memory _salt,
-        address _erc20Token,
-        uint _amount,
-        address _merchantAddress,
-        address _purplePayMultiSig
-    ) public view onlyOwner returns (address) {
-        bytes memory nativeContractBytecode = abi.encodePacked(
-            type(NativeBurnerContract).creationCode,
-            abi.encode(_amount),
-            abi.encode(_merchantAddress),
-            abi.encode(_purplePayMultiSig)
-        );
+	function predictAddress(
+		string memory _salt,
+		address _erc20Token,
+		uint _amount,
+		address _merchantAddress,
+		address _purplePayMultiSig
+	) public view onlyOwner returns (address) {
+		bytes memory nativeContractBytecode = abi.encodePacked(
+			type(NativeBurnerContract).creationCode,
+			abi.encode(_amount),
+			abi.encode(_merchantAddress),
+			abi.encode(_purplePayMultiSig)
+		);
 
-        bytes memory erc20ContractBytecode = abi.encodePacked(
-            type(ERC20BurnerContract).creationCode,
-            abi.encode(_erc20Token),
-            abi.encode(_amount),
-            abi.encode(_merchantAddress),
-            abi.encode(_purplePayMultiSig)
-        );
+		bytes memory erc20ContractBytecode = abi.encodePacked(
+			type(ERC20BurnerContract).creationCode,
+			abi.encode(_erc20Token),
+			abi.encode(_amount),
+			abi.encode(_merchantAddress),
+			abi.encode(_purplePayMultiSig)
+		);
 
-        bytes memory contractBytecode = _erc20Token == address(0)
-            ? nativeContractBytecode
-            : erc20ContractBytecode;
+		bytes memory contractBytecode = _erc20Token == address(0)
+			? nativeContractBytecode
+			: erc20ContractBytecode;
 
-        bytes32 hash = keccak256(
-            abi.encodePacked(
-                bytes1(0xff),
-                address(this),
-                bytes32(keccak256(abi.encodePacked(_salt))),
-                keccak256(contractBytecode)
-            )
-        );
+		bytes32 hash = keccak256(
+			abi.encodePacked(
+				bytes1(0xff),
+				address(this),
+				bytes32(keccak256(abi.encodePacked(_salt))),
+				keccak256(contractBytecode)
+			)
+		);
 
-        return (address(uint160(uint(hash))));
-    }
+		return (address(uint160(uint(hash))));
+	}
 }
